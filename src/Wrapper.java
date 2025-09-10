e4a6a26be2ae2e300474888db7f49c8511ff852b
gavendra123-bit <pachahragavendra@gmail.com>
2025-09-10 13:50:27 +0530

    code

---
+++ src/Wrapper.java
@@ -0,0 +1,9 @@
+public class Wrapper {
+    public static void main(String args[]){
+        int x=20;
+        Integer y = x;
+        System.out.println(y);
+        int z=y;
+        System.out.println(z);
+    }
+}
